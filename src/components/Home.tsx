3b0e278ec6fd6727c0e251b85e2ac1fc59dbf81c
derrickbass2 <derrick.bass2@gmail.com>
2025-01-10 22:53:02 -0500

    Remove unused files and add initial Home component implementation

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./Hero";
+import Process from "./Process";
+
+const Home: React.FC = () => (
+  <div>
+    <Hero />
+    <Process />
+  </div>
+);
+
+export default Home;
--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './process.css';
 
 const Process = () => {
