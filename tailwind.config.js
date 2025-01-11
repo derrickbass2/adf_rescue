72f0e4af743b00659747bbd49cabc8825d445abb
derrickbass2 <derrick.bass2@gmail.com>
2025-01-10 19:09:13 -0500

    Save changes to package.json and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
 module.exports = {
-  content: [],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-=======
-<<<<<<< HEAD
-module.exports = {content: ["./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",], theme: {
-    extend: {},
-  }, plugins: [],}
-=======
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
->>>>>>> 6e84c45 (Add initial project files, including backend models, frontend components, and configuration)
->>>>>>> 79a68a5 (Add initial project files, including backend models, frontend components, and configuration)
 
